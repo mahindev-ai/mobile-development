77c4376187f8ce32755d7aa47fed4eaf46066155
mahiinn <mariaperezb03@gmail.com>
2023-03-07 23:56:19 -0500

    resolve problems with variables null

--- class01.dart
+++ class01.dart
@@ -1,11 +1,10 @@
-import 'dart:io'; // Paquete necesario para poder leer entradas por consola. 
+// import 'dart:io'; // Paquete necesario para poder leer entradas por consola.
 
-class Heroe{
-    
-  String nombre;
-  String poder;
-  
-  Heroe ({String nombre, String poder}){
+class Heroe {
+  String nombre = '';
+  String poder = '';
+
+  Heroe({required String nombre, required String poder}) {
     this.nombre = nombre;
     this.poder = poder;
   }
@@ -19,10 +18,10 @@ void main() {
   //     print('Es par $i');
   //   }
   // }
-  
+
   // print('Digite un numero');
   // int num2 = int.parse(stdin.readLineSync());
-  
+
   // if(num2>0){
   //   print('El numero $num2 es positivo');
   // }else if (num2<0){
@@ -31,44 +30,39 @@ void main() {
   //   print('El numero numero es 0');
   // }
 
-  Map <String, dynamic> persona = {
-    'nombre': 'Max',
-    'edad': 33,
-    'soltero': true
-  };
+  Map<String, dynamic> persona = {'nombre': 'Max', 'edad': 33, 'soltero': true};
   // Para agregar atributos nuevos
   persona.addAll({'Sexo': 'Masculino'});
-  
+
   // Mostrar un atributo en especifico
   print(persona['nombre']);
-  
+
   // Mostrar todo los datos del mapa
   print(persona);
 
   // Creación de una función.
-  String saludar1(String texto, String nombre){
+  String saludar1(String texto, String nombre) {
     return '$texto $nombre';
   }
-  String mensaje1 = saludar1 ('Hola','Maria');
+
+  String mensaje1 = saludar1('Hola', 'Maria');
 
   print(mensaje1);
-  
-  // Parámetro con nombre, sin importar la posición en la que coloques los datos, la función siempre obligará al orden. La diferencia principal es que los parametros estarán entre {} y luego al momento de llamar la función tenemos que mandar a qué parámetro está asociado. 
-  String saludar2({String texto, String nombre}){
+
+  // Parámetro con nombre, sin importar la posición en la que coloques los datos, la función siempre obligará al orden. La diferencia principal es que los parametros estarán entre {} y luego al momento de llamar la función tenemos que mandar a qué parámetro está asociado.
+  String saludar2({String? texto, String? nombre}) {
     return '$texto $nombre';
-  };
-  
-  // Aquí se asocian los parámetros para poder forzar el orden. 
-  String mensaje2 = saludar2(nombre:'Hola',texto: 'Maria');
-  
-  print (mensaje2);
+  }
+
+  // Aquí se asocian los parámetros para poder forzar el orden.
+  String mensaje2 = saludar2(nombre: 'Hola', texto: 'Maria');
+
+  print(mensaje2);
 
   // Manejo de Clases
 
-  
   // Instancia de la clase Heroe
   final heroe = new Heroe(nombre: 'Superman', poder: 'Volar');
-  
-  print(heroe.nombre +" puede " + heroe.poder);
-  
+
+  print(heroe.nombre + " puede " + heroe.poder);
 }
